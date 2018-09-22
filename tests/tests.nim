03a5bad968ea54b1fcba3742376ce092f5e98928
Timothee Cour <timothee.cour2@gmail.com>
2018-09-22 13:57:18 +0200

    _

--- tests/tests.nim
+++ tests/tests.nim
@@ -3,16 +3,16 @@ from sequtils import toSeq
 import sugar
 
 proc test1()=
-  for i in filter(map(filter(2..10, proc(x: int): bool = x mod 2 == 0),
-           proc(x: int): int = x * 2), proc(x: int): bool = x mod 8 == 0):
-    echo i
+   for i in filter(map(filter(2..10, proc(x: int): bool = x mod 2 == 0),
+            proc(x: int): int = x * 2), proc(x: int): bool = x mod 8 == 0):
+     echo i
 
-  var s = toSeq(2..10).filter(x:int => x mod 2 == 0).
-      map(x => x * 2).filter(x => x mod 8 == 0)
-  echo s
+   var s = toSeq (2..10).filter(proc(x): bool = x mod 2 == 0).
+           map(proc(x): int = x * 2).filter(proc(x): bool = x mod 8 == 0)
+   echo s
 
-  var a = (2..10).filter(proc(x: int): bool = x mod 2 == 0)
-  var b = a.map((x: int) => x * 2)
-  for i in b.map((x: int) => x + 2):
-    echo i
+   var a = (2..10).filter(proc(x: int): bool = x mod 2 == 0)
+   var b = a.map((x: int) => x * 2)
+   for i in b.map((x: int) => x + 2):
+     echo i
 test1()
